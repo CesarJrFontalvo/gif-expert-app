67a574c822d85b36901252f71269b9cf27f4cb3a
Cesar Fontalvo <juniorfontalvo5@gmail.com>
2023-03-10 09:18:03 -0500

    first commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,12 @@
+export const getGif = async (category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=4Yy9ljABcFFs8h3htYfQfqCn0gLSOMNv&q=${category}&limit=10`
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+
+    const gifs = data.map(img => ({
+        id: img.id,
+        title: img.title,
+        url: img.images.downsized_medium.url
+    }))
+    return gifs;
+}
